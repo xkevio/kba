a93562ba6f619c8a65c8c266be6ad7ac28ff9c62
xkevio <xkeviota@gmail.com>
2023-09-07 00:06:11 +0200

    fix: don't forget closing brackets

--- build.rs
+++ build.rs
@@ -4,7 +4,7 @@ fn main() {
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("instructions.rs");
 
-    let prelude = "pub const INSTRUCTIONS: [u32; 4096] = [";
+    let prelude = "pub const INSTRUCTIONS: [u32; 4096] = [\n";
     let mut instrs = String::new();
 
     // Bits 20-27 and 4-7 are used to index the opcode (2^12 = 4096).
@@ -12,6 +12,6 @@ fn main() {
         instrs += &format!("{}", 0xFFFF_FFFFu32 & 0x0FF0_00F0);
     }
 
-    std::fs::write(&dest_path, prelude.to_string() + &instrs).unwrap();
+    std::fs::write(&dest_path, prelude.to_string() + &instrs + "\n];").unwrap();
     println!("cargo:rerun-if-changed=build.rs");
 }
