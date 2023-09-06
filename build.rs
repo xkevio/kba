c19ddde9b3e73f1ad1ed6a20079fc4f65221d5d1
xkevio <xkeviota@gmail.com>
2023-09-07 01:32:01 +0200

    feat: begin data processing instructions

--- build.rs
+++ build.rs
@@ -4,12 +4,13 @@ fn main() {
     let out_dir = std::env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("instructions.rs");
 
-    let prelude = "pub const INSTRUCTIONS: [u32; 4096] = [\n";
+    let prelude = "pub const INSTRUCTIONS: [fn(&mut Arm7TDMI); 4096] = [\n";
     let mut instrs = String::new();
 
     // Bits 20-27 and 4-7 are used to index the opcode (2^12 = 4096).
     for _ in 0..4096 {
-        instrs += &format!("{},\n", 0xFFFF_FFFFu32 & 0x0FF0_00F0);
+        // TODO: insert fn pointers.
+        // instrs += &format!("{},\n", ...);
     }
 
     std::fs::write(&dest_path, prelude.to_string() + &instrs + "\n];").unwrap();
--- src/arm/interpreter/arm7tdmi.rs
+++ src/arm/interpreter/arm7tdmi.rs
@@ -39,3 +39,68 @@ bitfield! {
         pub n: bool @ 31,
     }
 }
+
+impl Arm7TDMI {
+    /// If I is false, operand 2 is a register and gets shifted.
+    /// Otherwise, it is an unsigned 8 bit immediate value.
+    pub fn barrel_shifter(&self, op: u16, imm: bool) -> u32 {
+        if imm {
+            ((op & 0xFF) as u32).rotate_right((op as u32 & 0x0F00) * 2)
+        } else {
+            let rm = self.regs[op as usize & 0xF];
+            let shift = op & 0x0FF0;
+            todo!()
+        }
+    }
+
+    pub fn cond<const COND: u8>(&self) -> bool {
+        match COND {
+            0b0000 => self.cspr.z(),
+            0b0001 => !self.cspr.z(),
+            0b0010 => self.cspr.c(),
+            0b0011 => !self.cspr.c(),
+            0b0100 => self.cspr.n(),
+            0b0101 => !self.cspr.n(),
+            0b0110 => self.cspr.v(),
+            0b0111 => !self.cspr.v(),
+            0b1000 => self.cspr.c() && !self.cspr.z(),
+            0b1001 => self.cspr.c() && self.cspr.z(),
+            0b1010 => self.cspr.n() == self.cspr.v(),
+            0b1011 => self.cspr.n() != self.cspr.v(),
+            0b1100 => !self.cspr.z() && (self.cspr.n() == self.cspr.v()),
+            0b1101 => self.cspr.z() || (self.cspr.n() != self.cspr.v()),
+            0b1110 => true,
+            _ => unreachable!()
+        }
+    }
+
+    pub fn data_processing<const COND: u8, const I: bool, const S: bool>(&mut self, opcode: u32) {
+        let rn = self.regs[opcode as usize & 0x000F_0000];
+        let rd = self.regs[opcode as usize & 0xF000];
+        let op2 = self.barrel_shifter(opcode as u16, I);
+
+        // TODO: carry out from barrel shifter
+        if self.cond::<COND>() {
+            let result = match (opcode & 0x01E0_0000) >> 21 {
+                0b0000 => rn & op2,
+                0b0001 => rn ^ op2,
+                0b0010 => rn - op2,
+                0b0011 => op2 - rn,
+                0b0100 => rn + op2,
+                0b0101 => rn + op2 + self.cspr.c() as u32,
+                0b0110 => rn - op2 + self.cspr.c() as u32 - 1,
+                0b0111 => op2 - rn + self.cspr.c() as u32 - 1,
+                0b1000 => {
+                    if S {
+                        let tst = rn & op2;
+                        if tst == 0 { self.cspr.set_z(true) }
+                        self.cspr.set_c((tst & (1 << 31)) != 0);
+                    }
+
+                    rd
+                }
+                _ => todo!()
+            };
+        }
+    }
+}
