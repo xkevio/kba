42ed7e5665f037c11535835886149525e5c858ff
xkevio <xkeviota@gmail.com>
2023-12-26 17:49:11 +0100

    refactor: sdl init out of main

---
+++ src/frontend.rs
@@ -0,0 +1,122 @@
+use paste::paste;
+use sdl2::{
+    event::Event,
+    keyboard::Scancode,
+    pixels::PixelFormatEnum,
+    render::{Canvas, Texture, TextureCreator},
+    video::{Window, WindowContext},
+    EventPump,
+};
+
+use crate::{
+    gba::{Gba, LCD_HEIGHT, LCD_WIDTH},
+    ppu, SdlResult,
+};
+
+macro_rules! process_scancodes {
+    ($kba:expr, $state:expr; $($name:ident => $code:ident),*) => {
+        paste! {
+            $(
+                if $state.is_scancode_pressed(Scancode::$code) {
+                    $kba.cpu.bus.key_input.[<set_ $name>](false);
+                }
+            )*
+        }
+    };
+}
+
+pub struct SDLApplication {
+    event_pump: EventPump,
+    canvas: Canvas<Window>,
+    texture_creator: TextureCreator<WindowContext>,
+}
+
+impl SDLApplication {
+    pub fn new(title: &str) -> SdlResult<Self> {
+        let sdl_context = sdl2::init()?;
+        let video_subsystem = sdl_context.video()?;
+
+        let window = video_subsystem
+            .window(title, LCD_WIDTH as u32 * 2, LCD_HEIGHT as u32 * 2)
+            .position_centered()
+            .build()
+            .map_err(|e| e.to_string())?;
+
+        let event_pump = sdl_context.event_pump()?;
+        let canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
+        let texture_creator = canvas.texture_creator();
+
+        Ok(Self {
+            event_pump,
+            canvas,
+            texture_creator,
+        })
+    }
+
+    pub fn run(&mut self, kba: &mut Gba) -> SdlResult<()> {
+        let mut texture = self
+            .texture_creator
+            .create_texture_streaming(PixelFormatEnum::RGBA32, LCD_WIDTH as u32, LCD_HEIGHT as u32)
+            .map_err(|e| e.to_string())?;
+
+        // Actual loop that runs the program and the emulator.
+        'main: loop {
+            for event in self.event_pump.poll_iter() {
+                if let Event::Quit { .. } = event {
+                    break 'main;
+                }
+            }
+
+            let keyboard_state = self.event_pump.keyboard_state();
+            process_scancodes!(kba, keyboard_state;
+                up => Up,
+                left => Left,
+                down => Down,
+                right => Right,
+                start => Return,
+                select => Backspace,
+                a => X,
+                b => Y,
+                l => A,
+                r => S
+            );
+
+            // todo: vsync delay / sleep.
+            // For now, update every 266_666 cycles (60 frames).
+            while kba.cycles < 266_666 {
+                kba.run();
+            }
+
+            // Update frame and convert Option pixel values to corresponding colors.
+            self.update_texture(
+                &mut texture,
+                &kba.cpu.bus.ppu.buffer[0..(LCD_WIDTH * LCD_HEIGHT)],
+            )?;
+
+            kba.cycles = 0;
+            kba.cpu.bus.key_input.set_keyinput(0xFFFF);
+
+            self.canvas.clear();
+            self.canvas.copy(&texture, None, None)?;
+            self.canvas.present();
+        }
+
+        Ok(())
+    }
+
+    fn update_texture(&self, texture: &mut Texture, buffer: &[Option<u16>]) -> SdlResult<()> {
+        texture.with_lock(None, |buf: &mut [u8], _: usize| {
+            for (i, px) in buffer[0..(LCD_WIDTH * LCD_HEIGHT)].iter().enumerate() {
+                let [r, g, b, a] = match px {
+                    Some(color) => ppu::rgb555_to_color(*color).to_be_bytes(),
+                    None => [0, 0, 0, 255],
+                };
+
+                buf[i * 4] = r;
+                buf[i * 4 + 1] = g;
+                buf[i * 4 + 2] = b;
+                buf[i * 4 + 3] = a;
+            }
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,24 @@
 #![allow(dead_code)]
-
 use std::path::Path;
 
-use gba::{Gba, LCD_HEIGHT, LCD_WIDTH};
-use paste::paste;
-use sdl2::{event::Event, keyboard::Scancode, pixels::PixelFormatEnum};
+use frontend::SDLApplication;
+use gba::Gba;
 
 mod arm;
+mod frontend;
 mod gba;
 mod mmu;
 mod ppu;
 
-type SdlResult<T> = Result<T, String>;
-
-macro_rules! process_scancodes {
-    ($kba:expr, $state:expr; $($name:ident => $code:ident),*) => {
-        paste! {
-            $(
-                if $state.is_scancode_pressed(Scancode::$code) {
-                    $kba.cpu.bus.key_input.[<set_ $name>](false);
-                }
-            )*
-        }
-    };
-}
+pub type SdlResult<T> = Result<T, String>;
 
 fn main() -> SdlResult<()> {
     let rom_path = std::env::args().nth(1).expect("A rom has to be specified!");
-    let file_name = Path::new(&rom_path)
-        .file_name()
-        .and_then(|r| r.to_str())
-        .unwrap_or_default();
+    let file_name = Path::new(&rom_path).file_name().unwrap_or_default();
     let rom = std::fs::read(&rom_path).map_err(|e| e.to_string())?;
 
+    let mut sdl_application = SDLApplication::new(&format!("κba - {:?}", file_name))?;
     let mut kba = Gba::with_rom(&rom);
 
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-
-    let window = video_subsystem
-        .window(
-            &format!("κba - {}", file_name),
-            LCD_WIDTH as u32 * 2,
-            LCD_HEIGHT as u32 * 2,
-        )
-        .position_centered()
-        .build()
-        .map_err(|e| e.to_string())?;
-
-    let mut canvas = window.into_canvas().build().map_err(|e| e.to_string())?;
-    let mut event_pump = sdl_context.event_pump()?;
-
-    let texture_creator = canvas.texture_creator();
-    let mut texture = texture_creator
-        .create_texture_streaming(PixelFormatEnum::RGBA32, LCD_WIDTH as u32, LCD_HEIGHT as u32)
-        .map_err(|e| e.to_string())?;
-
-    // Actual loop that runs the program and the emulator.
-    'main: loop {
-        for event in event_pump.poll_iter() {
-            if let Event::Quit { .. } = event {
-                break 'main;
-            }
-        }
-
-        let keyboard_state = event_pump.keyboard_state();
-        process_scancodes!(kba, keyboard_state;
-            up => Up,
-            left => Left,
-            down => Down,
-            right => Right,
-            start => Return,
-            select => Backspace,
-            a => X,
-            b => Y,
-            l => A,
-            r => S
-        );
-
-        // For now, update every 266_666 cycles (60 frames).
-        while kba.cycles < 266_666 {
-            kba.run();
-        }
-
-        // Update frame and convert Option pixel values to corresponding colors.
-        texture.with_lock(None, |buffer: &mut [u8], _: usize| {
-            for (i, px) in kba.cpu.bus.ppu.buffer[0..(LCD_WIDTH * LCD_HEIGHT)]
-                .iter()
-                .enumerate()
-            {
-                let [r, g, b, a] = match px {
-                    Some(color) => ppu::rgb555_to_color(*color).to_be_bytes(),
-                    None => [0, 0, 0, 255],
-                };
-
-                buffer[i * 4] = r;
-                buffer[i * 4 + 1] = g;
-                buffer[i * 4 + 2] = b;
-                buffer[i * 4 + 3] = a;
-            }
-        })?;
-
-        kba.cycles = 0;
-        kba.cpu.bus.key_input.set_keyinput(0xFFFF);
-
-        canvas.clear();
-        canvas.copy(&texture, None, None)?;
-        canvas.present();
-    }
-
-    Ok(())
+    sdl_application.run(&mut kba)
 }
